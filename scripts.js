ff27255b9257251f7d8f7e083e444405cfc53251
Ivan <ivan.dubrouski@gmail.com>
2018-05-03 16:36:26 +0300

    release v1

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+   var div = document.querySelectorAll('backspace');
+    div.addEventListener('keypress', lightining());
+
+     function lightining(){
+     div.style.backgroundColor=yellow; }
+
+
+
+
